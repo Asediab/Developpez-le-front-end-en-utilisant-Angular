8d4454d4d0c416f19e89db29bd8fd03296981c04
Bohdan <asediaboli@gmail.com>
2023-02-25 21:31:40 +0100

    Country Details page done

--- src/app/pages/country-details/country-details.component.ts
+++ src/app/pages/country-details/country-details.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {OlympicService} from "../../core/services/olympic.service";
-import {Router} from "@angular/router";
-import {OlympicDataLine} from "../../core/models/Interfaces";
+import {ActivatedRoute, Router} from "@angular/router";
+import {OlympicDataLine, Series} from "../../core/models/Interfaces";
 import {Olympic} from "../../core/models/Olympic";
 
 @Component({
@@ -29,23 +29,66 @@ export class CountryDetailsComponent implements OnInit {
   numberOfEntries!: number;
   numberOfMedals!: number;
   numberOfAthletes!: number;
-  olympicDataSetLine!: OlympicDataLine[];
+  olympicDataSetLine: OlympicDataLine[] = [];
   olympics!: Olympic[];
+  country!: Olympic;
+  countryId!: number;
 
   constructor(private olympicService: OlympicService,
-              private router: Router) {
+              private router: ActivatedRoute,
+              private rout: Router) {
   }
 
   ngOnInit(): void {
+    this.countryId = +this.router.snapshot.params['id'];
+    console.log(this.countryId);
+    try {
+      this.olympicService.getOlympics().subscribe(value => {
+        this.olympics = value;
+        this.country = value.find(value1 => value1.id === this.countryId) as Olympic;
+        if (!this.country) {
+          this.errorDataSetLoaded = true;
+        }
+        console.log(this.country);
+        this.getOlympicDataSetLine(this.country);
+        this.numberOfMedals = this.getNumberOfMedals(this.country);
+        console.log(this.numberOfMedals);
+        this.numberOfAthletes = this.getNumberOfAthletes(this.country);
+        console.log(this.numberOfAthletes);
+        this.numberOfEntries = this.getNumberOfEntries(this.country);
+        console.log(this.numberOfEntries);
+      });
+    } catch (e) {
+      this.errorDataSetLoaded = true;
+    }
+
+    this.errorDataSetLoaded = this.olympicService.error;
   }
 
-
-  onSelect(data: any): void {
+  private getOlympicDataSetLine(country: Olympic): void {
+    console.log(country);
+    let series: Series[] = country.participations.map(value => {
+      return new Series(value.medalsCount, value.year.toString());
+    })
+    console.log(series);
+    this.olympicDataSetLine.push(new OlympicDataLine(country.country, series));
+    this.multi = this.olympicDataSetLine;
+    console.log(this.multi)
   }
 
-  onActivate(data: any): void {
+  getNumberOfEntries(olympic: Olympic): number {
+    return olympic.participations.length;
   }
 
-  onDeactivate(data: any): void {
+  getNumberOfMedals(olympic: Olympic): number {
+    return olympic.participations.reduce((allMedals, previousValue) => allMedals + previousValue.medalsCount, 0);
+  }
+
+  getNumberOfAthletes(olympic: Olympic): number {
+    return olympic.participations.reduce((allMedals, previousValue) => allMedals + previousValue.athleteCount, 0);
+  }
+
+  onBack(): void {
+    this.rout.navigateByUrl("");
   }
 }
